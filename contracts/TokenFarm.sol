3975718307de662bf5ec8c19de5117fff7e85522
komdori <komdori@komdoris-MacBook-Pro.local>
2021-12-10 23:51:06 -0300

    add initial contract skeleton

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.10;
+
+contract TokenFarm {
+    
+    string public name = "Awesome Token Farm";
+    
+    //UsdtToken public usdtToken;
+
+    constructor() public {
+        
+    }
+}
